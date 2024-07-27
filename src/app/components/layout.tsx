41ce2d73a3d98d5dd7dc4643ed3f24b394654d80
arielf98 <arielfebrian98@gmail.com>
2024-07-27 19:36:00 +0700

    feat: menu buttons

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,15 +1,17 @@
 import { Menu } from "antd";
 import { Footer } from "antd/es/layout/layout";
 import React, { PropsWithChildren } from "react";
+import MenuButton from "./menu-button";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <div className='w-full h-screen bg-gray-200 flex justify-center flex-row'>
       <div
         className='bg-white flex items-center flex-col p-4 gap-4'
-        style={{ width: 480, height: "100dvh", overflow: "scroll" }}
+        style={{ width: 480, height: "100%", overflow: "scroll" }}
       >
         {children}
+        <MenuButton />
       </div>
     </div>
   );
--- src/app/components/menu-button.tsx
+++ src/app/components/menu-button.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import HomeOutlined from "@ant-design/icons/lib/icons/HomeOutlined";
+import { Button, Row } from "antd";
+import MenuOutlined from "@ant-design/icons/lib/icons/MenuOutlined";
+import UserOutlined from "@ant-design/icons/lib/icons/UserOutlined";
 
 export default function MenuButton() {
+  const MenuButtons = [
+    { title: "Home", icon: <HomeOutlined /> },
+    { title: "Menu", icon: <MenuOutlined /> },
+    { title: "Profile", icon: <UserOutlined /> },
+  ];
   return (
-    <div>
-        
-    </div>
-  )
+    <Row
+      className='bg-white fixed bottom-0 h-15 py-2 px-5 shadow-inner shadow-gray-100 w-full'
+      justify={"space-between"}
+    >
+      {MenuButtons.map((btn) => (
+        <Button>{btn.icon}</Button>
+      ))}
+    </Row>
+  );
 }
