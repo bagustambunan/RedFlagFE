76b2e48c94af4131405ecb79fec6eac7790c5c13
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2024-07-27 13:58:43 +0700

    feat: create page Register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -10,6 +10,34 @@ export default function page() {
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            placeholder="Name"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="phone"
+          >
+            Phone Number
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="phone"
+            type="text"
+            placeholder="Phone"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor="email"
           >
             Email
@@ -43,13 +71,13 @@ export default function page() {
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
           >
-            Sign In
+            Register
           </button>
           <a
             className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 py-2 px-4"
-            href="/register"
+            href="/login"
           >
-            Register
+            Sign In
           </a>
         </div>
       </form>
